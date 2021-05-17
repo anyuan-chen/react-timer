4c06ac4761aeee3f77e5415afbf80fe2635c9c5e
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-05-17 16:33:52 -0400

    unstyled timer done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Timer from "./Components/Timer";
 
 export default function App() {
   return (
     <div>
-      <h1>h1</h1>
+      <Timer></Timer>
     </div>
   );
 }
---
+++ src/Components/Timer.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+
+export default class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      timerOn: false,
+      timerStart: 0,
+      timerTime: 0 
+    };
+  }
+
+  startTimer  = () => {
+    this.setState({
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: Date.now() - this.state.timerTime
+    })
+
+    this.timer = setInterval(() => {
+      this.setState({
+        timerTime: Date.now() - this.state.timerStart
+      })
+    }, 10)
+  }
+
+  stopTimer = () => {
+    this.setState({timerOn: false});
+    clearInterval(this.timer);
+  }
+
+  resetTimer = () => {
+    this.setState({
+      timerStart: 0,
+      timerTime: 0
+    })
+  }
+
+  render() {
+
+    const {timerTime} = this.state;
+    let centiSeconds = ("0" + (Math.floor(timerTime/10)%100)).slice(-2)
+    let seconds = ("0" + (Math.floor(timerTime/1000)%60)).slice(-2)
+    let minutes = ("0" + (Math.floor(timerTime/60000)%60)).slice(-2)
+    let hours = ("0" + Math.floor(timerTime/3600000)).slice(-2)
+
+    return (
+      <div>
+        <h1>
+          {
+            timerTime > 60*60000 && (
+              <span>{hours}:</span>
+            )
+          }
+          
+          {
+            timerTime > 60000 && (
+              <span>{minutes}:</span>
+            )
+          }
+          
+          
+          
+          <span>{seconds}:</span>
+          <span>{centiSeconds}</span>
+        </h1>
+
+
+        {
+          this.state.timerOn === false && this.state.timerTime === 0 && (
+            <button onClick={this.startTimer}>Start</button>
+          )
+        }
+        {
+          this.state.timerOn === true && (
+            <button onClick={this.stopTimer}>Stop</button>
+          )
+        }
+        {
+          this.state.timerOn === false && this.state.timerTime != 0 && (
+            <button onClick={this.startTimer}>Resume</button>
+          )
+        }
+        {
+          this.state.timerOn === false && this.state.timerTime != 0 && (
+            <button onClick={this.resetTimer}>Reset</button>
+          )
+        }
+      </div>
+    );
+  }
+}
